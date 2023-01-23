f0088bb443e24d198d3b42a0e6574002b14d2785
Carolina <carolsalgadoguadanhini@gmail.com>
2023-01-23 20:01:32 -0300

    add employee dto

---
+++ spring-boot-api/src/main/java/com/company/api/dto/EmployeeDto.java
@@ -0,0 +1,22 @@
+package com.company.api.dto;
+
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
+public class EmployeeDto {
+    private String name;
+
+    private Integer age;
+
+    private String cpf;
+
+    private String phoneNumber;
+
+    private String position;
+
+    private String sector;
+    
+    private String salary;
+}

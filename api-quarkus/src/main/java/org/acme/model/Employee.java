80a3c2ec8d43e6287fd2226e2084a4d5b2625724
Carolina <carolsalgadoguadanhini@gmail.com>
2023-01-24 00:52:59 -0300

    change employee entity to quarkus framework

--- api-quarkus/src/main/java/org/acme/model/Employee.java
+++ api-quarkus/src/main/java/org/acme/model/Employee.java
@@ -1,8 +1,9 @@
-package com.company.api.model;
+package org.acme.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.Id;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
 import lombok.Data;
 
 @Entity
